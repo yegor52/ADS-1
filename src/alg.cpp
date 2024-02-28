b1bdcee7e3db779ec785910e61f44e75552d5e92
yegor52 <yegorbeat2005@gmail.com>
2024-02-29 01:01:12 +0300

    1

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,8 +19,8 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
     uint64_t count = 0;
     uint64_t chislo = 2;
-    while (count < n){
-        if(checkPrime(chislo)) {
+    while (count < n) {
+        if (checkPrime(chislo)) {
             count++;
         }
         chislo++;
@@ -30,7 +30,7 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
     value++;
-    while (!checkPrime(value)){
+    while (!checkPrime(value)) {
         value++;
     }
     return value;
@@ -39,8 +39,8 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t number = 2;
     uint64_t summa = 0;
-    while (number < hbound){
-        if (checkPrime(number)){
+    while (number < hbound) {
+        if (checkPrime(number)) {
             summa = summa + number;
         }
         number++;
