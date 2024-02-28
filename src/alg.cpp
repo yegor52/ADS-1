90b7ad1077635d5d31a58cb00e2741b2b8ed69c0
yegor52 <yegorbeat2005@gmail.com>
2024-02-29 00:51:01 +0300

    1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,49 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) {
+        return false;
+    }
+
+    for (uint64_t i = 2; i * i <= value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    uint64_t chislo = 2;
+    while (count < n){
+        if(checkPrime(chislo)) {
+            count++;
+        }
+        chislo++;
+    }
+    return chislo - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    value++;
+    while (!checkPrime(value)){
+        value++;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t number = 2;
+    uint64_t summa = 0;
+    while (number < hbound){
+        if (checkPrime(number)){
+            summa = summa + number;
+        }
+        number++;
+    }
+    return summa;
 }
